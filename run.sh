230fab301793a49ae1c63e79333c6cf8f2454598
MineServer <artdantes@yandex.ru>
2023-03-25 20:35:11 +0000

    modified run.sh script

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-# java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-43.2.3/unix_args.txt "$@" nogui;
-#cd FerraxWorld;
+java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-43.2.3/unix_args.txt "$@" nogui;
+cd FerraxWorld;
 name=$(date +"%Y%m%d_%H%M%S");
 git add -A
 git commit -a -m "offline-${name}";
